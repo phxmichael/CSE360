5525c73273d5d0e20b103f0ba7c2f2896a1e962c
phxmichael <michael.a.galvan@asu.edu>
2020-10-05 20:14:58 -0700

    Completed the methods for the AddingMachine.

--- eclipse-workspace/cse360assignment01/src/cse360assignment02/AddingMachine.java
+++ eclipse-workspace/cse360assignment01/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,44 @@
 package cse360assignment02;
 
 public class AddingMachine {
-	  private int total;
-	  
-	  public AddingMachine () {
-	    total = 0;  // not needed - included for clarity
-	  }
-	  
-	  public int getTotal () {
-	    return 0;
-	  }
-	  
-	  public void add (int value) {
-	  }
 
-	  public void subtract (int value) {
-	  }
+private int total;
+private String history;
+  
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+    history = Integer.toString(total);
+  }
+  
+  //The getTotal method returns the current total.
+  public int getTotal () {
+    return total;
+  }
+  
+  
+ //The add method adds the parameter to the total variable.
+  public void add (int value) {
+	  history += " + " + Integer.toString(value);		//Add change to history.
+	  total = value + total;							//Calculate new total
+  }
 
-	  public String toString () {
-	    return "";
-	  }
+  //The subtract method subtracts the parameter from the total variable
+  public void subtract (int value) {
+	  history += " - " + Integer.toString(value);		//Add change to history.
+	  total = total - value;							//Calculate new total.
+  }
 
-	  public void clear() {
-	  }
-	}
+  //This method returns the history
+  public String toString () {
+	  return history;
+  }
+
+  //This method clears the history.
+  public void clear() {
+	  history = "";
+  }
+}
+
+
+
